f3a0d5d417fd75e1dfd8a4286316490cd4b1ab07
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-07-30 02:04:52 +0300

    service list

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ import Main from "./Main/Main";
 
 import "./App.css";
 import Footer from "./Footer/Footer";
+import { styled } from "styled-components";
 
 function App() {
   return (
-    <>
+    <TimeDiv>
       <HeaderMobile />
       <Main />
       <Footer />
-    </>
+    </TimeDiv>
   );
 }
 
 export default App;
-// deploy
+
+const TimeDiv = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
