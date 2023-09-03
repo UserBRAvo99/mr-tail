bcda9b3174dc8055256ceaf3b5c7f190c5f576dc
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-09-03 18:05:08 +0300

    service

--- src/Data/dataServiceInfo.js
+++ src/Data/dataServiceInfo.js
@@ -1,27 +1,27 @@
-import photoDog1 from "./photoDog1.jpg";
-import photoDog2 from "./photoDog2.jpg";
-import photoDog3 from "./photoDog3.jpg";
+import photoDog1 from "./IMG_20230903_175158_644-01.jpeg";
+import photoDog2 from "./IMG_20230903_175148_875-01.jpeg";
+import photoDog3 from "./IMG_20230903_175211_527-01.jpeg";
 import photoDog4 from "./photoDog4.jpg";
-import photoDog5 from "./photoDog5.jpg";
-import photoDog6 from "./photoDog6.jpeg";
+// import photoDog5 from "./photoDog5.jpg";
+// import photoDog6 from "./photoDog6.jpeg";
 
 const dataServiceInfo = [
   {
     image: photoDog1,
     id: 1,
-    title: "Послуга",
+    title: "Послух",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
   },
   {
     image: photoDog2,
     id: 2,
-    title: "Послуга",
+    title: "Виїзні заняття",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
   },
   {
     image: photoDog3,
     id: 3,
-    title: "Послуга",
+    title: "Зоомагазин",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
   },
   {
@@ -30,18 +30,18 @@ const dataServiceInfo = [
     title: "Послуга",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
   },
-  {
-    image: photoDog5,
-    id: 5,
-    title: "Послуга",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
-  },
-  {
-    image: photoDog6,
-    id: 6,
-    title: "Послуга",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
-  },
+  // {
+  //   image: photoDog5,
+  //   id: 5,
+  //   title: "Послуга",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
+  // },
+  // {
+  //   image: photoDog6,
+  //   id: 6,
+  //   title: "Послуга",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in incidunt error tenetur vero? Quam, distinctio modi tempore fugit cumque sint, magnam odio debitis laudantium suscipit facere esse totam fuga.",
+  // },
 ];
 
 export default dataServiceInfo;
